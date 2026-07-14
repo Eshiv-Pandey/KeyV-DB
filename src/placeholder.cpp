5035beb80ab7820dc5d5b515ed2509d510c7a195
Eshiv Pandey <eshivpandey18@gmail.com>
2026-07-14 23:18:05 +0530

    adding placeholder.cpp to satisfy CMake's requirements

---
+++ src/placeholder.cpp
@@ -0,0 +1,7 @@
+// placeholder.cpp
+//
+// This file exists solely to satisfy CMake's requirement that a static
+// library target has at least one source file. It will be removed once
+// the first real source (disk_manager.cpp) is added in Milestone 1.
+//
+// Do not put any logic here.
